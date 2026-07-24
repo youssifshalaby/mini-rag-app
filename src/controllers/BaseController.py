67bf9bde15590d1082e63023cca8dc7bbf2f336d
youssif <youssifshalabe@gmail.com>
2026-07-24 22:50:27 +0300

    check

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,5 +1,7 @@
 from helpers.config import get_settings, Settings
-
+import os
 class BaseController:
     def __init__(self):
         self.app_settings = get_settings()
+        self.base_dir = os.path.dirname(os.path.dirname(__file__))
+        self.files_dir = os.path.join(self.base_dir,"assets/files")
