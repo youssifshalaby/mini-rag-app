cf7c3a533f54738febadf694e4002665bd444bd9
YoussefShalaby <youssifshalabe@gmail.com>
2026-07-27 20:36:59 +0300

    After reveiw

--- src/controllers/ProjectController.py
+++ src/controllers/ProjectController.py
@@ -1,6 +1,4 @@
 from .BaseController import BaseController
-from fastapi import UploadFile
-from models import ResponseSignal
 import os
 class ProjectController(BaseController):
     def __init__(self):
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController,ProjectController
+from controllers import DataController
 import aiofiles
-import os
 from models import ResponseSignal
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -11,8 +10,9 @@ data_router = APIRouter(
 )
 
 @data_router.post("/upload/{project_id}")
-async def upload_data(project_id:str,file:UploadFile,
+async def upload_data(project_id:str , file:UploadFile,
                       app_settings: Settings = Depends(get_settings)):
+    
     data_controller = DataController()
     is_valid, signal_result = data_controller.validate_uploaded_file(file=file)
     if not is_valid:
@@ -23,7 +23,6 @@ async def upload_data(project_id:str,file:UploadFile,
             }
         )
 
-    project_dir_path = ProjectController().get_project_path(project_id=project_id)
     file_path = data_controller.generate_unique_filename(
         original_file_name=file.filename,
         project_id=project_id)
