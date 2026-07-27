8614579ac79bbad3bc738b22e11d22bafc983b38
YoussefShalaby <youssifshalabe@gmail.com>
2026-07-27 19:39:35 +0300

    finish file uploading

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,7 +1,9 @@
 from .BaseController import BaseController
+from .ProjectController import ProjectController
 from fastapi import UploadFile
 from models import ResponseSignal
 import os
+import re
 class DataController(BaseController):
     def __init__(self):
         super().__init__()
@@ -20,7 +22,19 @@ class DataController(BaseController):
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
             return False , ResponseSignal.FILE_SIZE_EXCEEDED.value
     
-        
-        
-
         return True , ResponseSignal.FILE_VALIDATED_SUCCESS.value
+
+    def generate_unique_filename(self,original_file_name: str,project_id: str):
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id = project_id)
+        cleaned_file_name = self.get_clean_file_name(original_file_name=original_file_name)
+        new_file_path = os.path.join(project_path,random_key + "_" + cleaned_file_name)
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(project_path,random_key + "_" + cleaned_file_name)
+        return new_file_path
+
+    def get_clean_file_name(self,original_file_name:str):
+        cleaned_file_name = re.sub(r"[^\w.]","",original_file_name.strip())
+        cleaned_file_name = cleaned_file_name.replace(" ","_")
+        return cleaned_file_name
--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     APP_VERSION: str
     FILE_ALLOWED_TYPES: list[str]
     FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
 
     model_config = ConfigDict(env_file=".env")
     
